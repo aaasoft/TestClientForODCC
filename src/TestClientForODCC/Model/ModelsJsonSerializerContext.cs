a000643dfacbbd415f0730ac22960eff15fac057
张鹏 <scbeta@qq.com>
2026-06-04 12:03:48 +0800

    增加获取配置和获取在线数据功能。

--- src/TestClientForODCC/Model/ModelsJsonSerializerContext.cs
+++ src/TestClientForODCC/Model/ModelsJsonSerializerContext.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Text.Encodings.Web;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace TestClientForODCC.Model;
@@ -6,7 +7,17 @@ namespace TestClientForODCC.Model;
 
 [JsonSerializable(typeof(ODCCRequest<LoginRequest>))]
 [JsonSerializable(typeof(ODCCResponse<LoginResponse>))]
+[JsonSerializable(typeof(ODCCRequest<GetConfigRequest>))]
+[JsonSerializable(typeof(ODCCResponse<GetConfigResponse>))]
+[JsonSerializable(typeof(ODCCRequest<GetOnlineDataRequest>))]
+[JsonSerializable(typeof(ODCCResponse<GetOnlineDataResponse>))]
+[JsonSerializable(typeof(SpaceInfo[]))]
+[JsonSerializable(typeof(DeviceData[]))]
 public partial class ModelsJsonSerializerContext : JsonSerializerContext
 {
-
+    public static ModelsJsonSerializerContext Default2 { get; } = new ModelsJsonSerializerContext(new JsonSerializerOptions()
+    {
+        WriteIndented = true,
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    });
 }
--- src/TestClientForODCC/Program.cs
+++ src/TestClientForODCC/Program.cs
@@ -29,7 +29,7 @@ var httpClent = new HttpClient()
 {
     Console.WriteLine("正在登录...");
     var apiUrl = $"{url}/north/login";
-    var loginRequest = new ODCCRequest<LoginRequest>()
+    var request = new ODCCRequest<LoginRequest>()
     {
         version = "",
         data = new()
@@ -40,14 +40,15 @@ var httpClent = new HttpClient()
     };
     try
     {
-        var loginResponse = await HttpUtils.Post(httpClent, apiUrl, loginRequest,
-            ModelsJsonSerializerContext.Default.ODCCRequestLoginRequest,
-            ModelsJsonSerializerContext.Default.ODCCResponseLoginResponse);
-        Console.WriteLine($"登录结果: error_code: {loginResponse.error_code}, error_msg: {loginResponse.error_msg}, login_time: {loginResponse.data.login_time}, timeout: {loginResponse.data.timeout}");
+        var response = await HttpUtils.Post(httpClent, apiUrl, request,
+            ModelsJsonSerializerContext.Default2.ODCCRequestLoginRequest,
+            ModelsJsonSerializerContext.Default2.ODCCResponseLoginResponse);
+        Console.WriteLine($"响应: error_code: {response.error_code}, error_msg: {response.error_msg}");
+        Console.WriteLine($"登录结果: login_time: {response.data.login_time}, timeout: {response.data.timeout}");
     }
     catch (Exception ex)
     {
-        Console.WriteLine($"将JSON数据反序列化为对象时出错，原因：{ex}");
+        Console.WriteLine($"登录时出错，原因：{ex}");
         return;
     }
 }
@@ -57,15 +58,75 @@ while (true)
     Console.WriteLine("请选择：");
     var selectId = QbSelect.ArrowSelect(new Dictionary<string, string>()
     {
-        ["1"] = "在线数据获取",
+        ["1"] = "配置获取",
+        ["2"] = "在线数据获取",
         ["0"] = "退出",
     }.ToArray());
     switch (selectId)
     {
         case "0":
             return;
+        //配置获取
         case "1":
             {
+                var apiUrl = $"{url}/north/config_get";
+                var request = new ODCCRequest<GetConfigRequest>()
+                {
+                    version = "",
+                    data = new()
+                    {
+                        version = ""
+                    }
+                };
+                try
+                {
+                    var response = await HttpUtils.Post(httpClent, apiUrl, request,
+                        ModelsJsonSerializerContext.Default2.ODCCRequestGetConfigRequest,
+                        ModelsJsonSerializerContext.Default2.ODCCResponseGetConfigResponse);
+                    Console.WriteLine($"响应: error_code: {response.error_code}, error_msg: {response.error_msg}");
+                    Console.WriteLine($"配置: {JsonSerializer.Serialize(response.data.nodes, ModelsJsonSerializerContext.Default2.SpaceInfoArray)}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"获取配置时出错，原因：{ex}");
+                    return;
+                }
+                break;
+            }
+        //在线数据获取
+        case "2":
+            {
+                Console.Write("位置编号:");
+                var space_guid = Console.ReadLine();
+                Console.Write("设备编号:");
+                var device_guid = Console.ReadLine();
+                Console.Write("点位编号:");
+                var point_guid = Console.ReadLine();
+
+                var apiUrl = $"{url}/north/online_data_get";
+                var request = new ODCCRequest<GetOnlineDataRequest>()
+                {
+                    version = "",
+                    data = new()
+                    {
+                        space_guids = string.IsNullOrEmpty(space_guid) ? null : space_guid.Split(','),
+                        device_guids = string.IsNullOrEmpty(device_guid) ? null : device_guid.Split(','),
+                        point_guids = string.IsNullOrEmpty(point_guid) ? null : point_guid.Split(','),
+                    }
+                };
+                try
+                {
+                    var response = await HttpUtils.Post(httpClent, apiUrl, request,
+                        ModelsJsonSerializerContext.Default2.ODCCRequestGetOnlineDataRequest,
+                        ModelsJsonSerializerContext.Default2.ODCCResponseGetOnlineDataResponse);
+                    Console.WriteLine($"响应: error_code: {response.error_code}, error_msg: {response.error_msg}");
+                    Console.WriteLine($"数据: {JsonSerializer.Serialize(response.data.devices, ModelsJsonSerializerContext.Default2.DeviceDataArray)}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"获取在线数据时出错，原因：{ex}");
+                    return;
+                }
                 break;
             }
     }
